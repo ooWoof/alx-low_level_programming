4b10341bc478fe764eccf247a3e140f87c5d408b
ooWoof <ricobricko7102@gmail.com>
2023-06-16 13:58:20 +0300

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h
+#include <stdio.h>
 /**
  * main - printing a line using puts function
- * Return(0):
+ * Return: (0)
  */
 int main(void)
 {
 	puts ("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
 
