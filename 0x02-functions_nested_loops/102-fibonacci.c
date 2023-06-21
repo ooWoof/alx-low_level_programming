f0c96e6d9cca47199057d2aea387f25796740bb4
ooWoof <ricobricko7102@gmail.com>
2023-06-21 06:37:30 -0700

    net

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main - Prints first 50 Fibonacci numbers, starting with 1 and 2
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
@@ -11,13 +13,17 @@ int main(void)
 
 	for (count = 0; count < 50; count++)
 	{
+		sum = fib1 + fib2;
 		printf("%lu", sum);
+
 		fib1 = fib2;
 		fib2 = sum;
+
 		if (count == 49)
 			printf("\n");
 		else
 			printf(", ");
 	}
+
 	return (0);
 }
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,46 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- *initialize the first two Fibonacci numbers
- *print the first two Fibonacci numbers
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
  */
+
 int main(void)
 {
-	int fib1 = 1;
-	int fib2 = 2;
-	int i;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	printf("%d, %d, ", fib1, fib2);
-	
-	for (i = 0; i < 98; i++)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		int fib3 = fib1 + fib2;
-		printf("%d, ", fib3);
-		fib1 = fib2;
-		fib2 = fib3;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
