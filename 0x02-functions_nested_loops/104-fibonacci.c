f7a2121fc8a9d5d5a18c1b1c46fc7025abe5688c
ooWoof <ricobricko7102@gmail.com>
2023-06-21 06:30:18 -0700

    net

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,13 +4,14 @@
  *initialize the first two Fibonacci numbers
  *print the first two Fibonacci numbers
  */
-int main()
+int main(void)
 {
 	int fib1 = 1;
 	int fib2 = 2;
 	int i;
 
 	printf("%d, %d, ", fib1, fib2);
+	
 	for (i = 0; i < 98; i++)
 	{
 		int fib3 = fib1 + fib2;
